d8a461ba3d1fec7d49b5996746e2cb351790a722
heyihong <983899480@qq.com>
2021-04-04 17:57:12 +0800

    创建工程git-test

---
+++ src/main/java/com/bjpowernode/git/Test.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.git;
+
+/**
+ * 何怡宏
+ * 2021/4/4
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
